7bf1cbd19039ecc1cec9d6fbc298119b13a7dbbd
Baldev Desai <baldevd13@gmail.com>
2020-07-26 14:42:35 -0400

    First try

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var port = process.env.PORT || 1337;
 
 var connectionPool = mysql.createPool({
     connectionLimit: 10,
-    host: "database-dept-dietdatabase.cloudapps.unc.edu",
-    user: "root",
+    host: "remotemysql.com",
+    user: process.env.user,
     password: process.env.ROOTPASS,
-    database: "birddietdb"
+    database: process.env.database
 });
 
 app.use(cors())
@@ -43,11 +43,22 @@ app.get('/prey_order', (req, res, next) => {
     });
 });
 
+app.get('/prey_order', (req, res, next) => {
+    connectionPool.getConnection(function (err, connection) {
+        connection.query('SELECT unique(Prey_Order) FROM db ORDER BY ', function (error, results, fields) {
+            res.json(results);
+            connection.release();
+        });
+    });
+});
+
+// SELECT * FROM birddietdb WHERE Common_Name = 'Bald eagle'
+
 app.post('/bird_search', (req, res, next) => {
     connectionPool.getConnection(function (err, connection) {
         console.log(req.query.bird);
         connection.query({
-            sql: 'INSERT INTO `feedback`(`feedback`) VALUES (?)',
+            sql: 'SELECT * FROM birddietdb WHERE Common_Name = "?"',
             timeout: 10000,
             values: [req.query.bird]
         }, function (error, results, fields) {
@@ -56,3 +67,48 @@ app.post('/bird_search', (req, res, next) => {
         });
     });
 });
+
+/* Variables
+*  @Common_Name VARCHAR(255)
+*  @Prey_Taxon_Level ('Kingdom', 'Phylum', 'Class', 'Order', 'Suborder', 'Family', 'Genus', 'Species')
+*  @Diet_Type ('Item', 'Wt_or_Vol', 'Occurrence', 'Unspecified') 
+*  @Season ('Fall', 'Winter', 'Spring', 'Summer', 'Any')
+*  @Region VARCHAR(255)
+*  @Year_Begin INT
+*  @Year_End INT
+*
+*  SELECT * FROM birddietdb WHERE Common_Name = @Common_Name          
+*                           AND Observation_Year_Begin >= @Year_Begin (0 if unspecified)   `filter1`
+*                           AND Observation_Year_End <= @Year_End (2100 if unspecified)     All records for a bird (may be multiple studies)
+*
+*
+*                   |
+*                   |  performing another query on the table from the previous query
+*                   V
+*
+*
+*  SELECT * FROM `filter1` WHERE Diet_Type IN @Diet_Type
+*                          AND Observation_Season IN @Season   `filter2`
+*                          AND Location_Region = @Region        Further filters records from `filter1` 
+*
+*  SELECT UNIQUE(Source, 
+*                Observation_Year_Begin, 
+*                Observation_Month_Begin, 
+*                Observation_Season, 
+*                Bird_Sample_Size,                      `Analyses`
+*                Habitat_Type,                           Represents different studies of the same species    
+*                Location_Region, 
+*                Item_Sample_Size, 
+*                Diet_Type, 
+*                Study_Type) FROM `filter2`
+*
+*
+*  SELECT COUNT* FROM `Analyses`      `numAnalyses`     The number of distinct studies for a given bird species 
+*                                                       (Needed for weighted average of occurrence studies)
+*
+*  SELECT Prey_Order, MAX(Fraction_Diet)/`numAnalyses` FROM `Analyses` WHERE Diet_Type = 'Occurrence'   if by Occurrence (Order is the default taxon)
+*                         
+*  SELECT Prey_Order, SUM(Fraction_Diet) FROM `Analyses` WHERE Diet_Type = 'Item'  if by Item (Order is the default taxon)
+*
+*
+*/

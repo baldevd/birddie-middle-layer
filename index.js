8ea6a386b71d2bde9e077b536fdf3d7989370205
Baldev Desai <baldevd13@gmail.com>
2020-07-27 15:12:26 -0400

    More advanced bird search added

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.get('/bird_search', (req, res, next) => {
     connectionPool.getConnection(function (err, connection) {
         console.log(req.query.bird);
         connection.query({
-            sql: 'SELECT * FROM `birddietdb` WHERE Common_Name = ?',
+            sql: 'SELECT Prey_Common_Name, Fraction_Diet, Diet_Type FROM `birddietdb` WHERE Common_Name = ?',
             timeout: 10000,
             values: [req.query.bird]
         }, function (error, results, fields) {
@@ -73,6 +73,20 @@ app.get('/advanced_bird_search', (req, res, next) => {
             sql: 'SELECT Prey_Common_Name, Fraction_Diet, Diet_Type FROM `birddietdb` WHERE Common_Name = ? AND Observation_Year_Begin >= ? AND Observation_Year_End <= ?',
             timeout: 10000,
             values: [req.query.bird, req.query.yearBegin, req.query.yearEnd]
+        }, function (error, results, fields) {
+            connection.release();
+            res.json(results)
+        });
+    });
+});
+
+app.get('/more_advanced_bird_search', (req, res, next) => {
+    connectionPool.getConnection(function (err, connection) {
+        console.log(req.query.bird, req.query.yearBegin, req.query.yearEnd, req.query.location, req.query.season);
+        connection.query({
+            sql: 'SELECT Prey_Common_Name, Fraction_Diet, Diet_Type FROM `birddietdb` WHERE Common_Name = ? AND Observation_Year_Begin >= ? AND Observation_Year_End <= ? AND Location_Region = ?, AND Observation_Season = ?',
+            timeout: 10000,
+            values: [req.query.bird, req.query.yearBegin, req.query.yearEnd, req.query.location, req.query.season]
         }, function (error, results, fields) {
             connection.release();
             res.json(results)
